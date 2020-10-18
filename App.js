5bb4565cd12ab9f86ab453361ba53ebe0fce7807
Hendry <hendry82@ui.ac.id>
2020-10-18 11:06:02 +0700

    Add Quiz3
    Finally final quiz

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+//import Tugas12 from "./tugas/tugas12/App";
+//import LoginScreen from "./tugas/tugas13/LoginScreen";
+//import AboutScreen from "./tugas/tugas13/AboutScreen";
+//import ToDoApp from "./tugas/tugas14/App";
+//import SkillScreen from "./tugas/tugas14/SkillScreen";
+//import Tugas15 from "./tugas/tugas15/index";
+//import Navigation from "./tugas/tugasNavigation/Navigation";
+import Quiz3 from "./tugas/quiz3/Navigation";
+
+export default function App() {
+  return <Quiz3 />;
+}
